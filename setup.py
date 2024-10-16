6e36cfb80dfd8a9cee528ed251dcda0a7e6f5bd5
Shun Bi <bishun1994@foxmail.com>
2024-10-16 08:38:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ import pyowt
 setup(
     name=pyowt.__package__,
     version=pyowt.__version__,
+    author='Shun Bi',
     packages=find_packages(),
     install_requires=[
         req.strip() for req in open('requirements.txt').readlines()
     ],
+    include_package_data=True,
+    package_data={
+        'pyowt': ['data/*']
+    },
+    license_files=('LICENSE')
 )
 
 # Please use the command to install the package in terminal
